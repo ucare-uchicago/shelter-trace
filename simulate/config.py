8a750b671bfb39df1e7d932b7fd9ea7f8f83e365
nlsandler <nlsandler@gmail.com>
2015-06-13 14:31:12 -0700

    organizing simulation scripts into one folder

---
+++ simulate/config.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("number", help="Trace number", type=str)
+parser.add_argument("-p", "---prealloc", help="""Configuration of preallocated shelter.""",
+                    type=int, choices=[1, 2, 3])
+
+parser.add_argument("-c", "---code", help="""Sheltering policy code.""",
+                    type=str, choices=["B", "C", "D", "E"])
+
+args = parser.parse_args()
